7c9c5594e17220700584396c3689bb97719d859d
Alyssa <alyssa.bawgus@students.olin.edu>
2013-03-14 16:34:48 -0400

    Added toggle to flashlight

--- Experiment2/Assets/LightController.js
+++ Experiment2/Assets/LightController.js
@@ -1,9 +1,15 @@
 #pragma strict
 
-function Start () {
-
-}
+var other : GameObject;
 
 function Update () {
+
+if (Input.GetKeyDown ("f")){
+	if (other.light.intensity != 0)
+		other.light.intensity = 0;
+	else 
+		other.light.intensity = .6;
+	
+}
 
 }
